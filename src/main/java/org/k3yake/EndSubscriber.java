3fcddb027f5ba3dd49213140e27bf74a45ff2c7c
katsuki.miyake <3katsu3hiro@gmail.com>
2018-03-21 21:38:42 +0900

    [WIP]サンプルを作成。まだテストは通っていない

---
+++ src/main/java/org/k3yake/EndSubscriber.java
@@ -0,0 +1,7 @@
+package org.k3yake;
+
+/**
+ * Created by katsuki-miyake on 18/03/19.
+ */
+public class EndSubscriber {
+}
---
+++ src/test/java/org/k3yake/EndSubscriberTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by katsuki-miyake on 18/03/19.
+ */
+public class EndSubscriberTest {
+
+}
